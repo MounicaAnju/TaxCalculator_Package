0dcca8b7590691d9834ef1ca6bfe24c5f8a2acfa
MounicaAnju <mounicapothureddy555@gmail.com>
2023-01-11 17:05:11 -0500

    Commit

---
+++ setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+setuptools.setup(
+        name="taxcalculatemounica", 
+        version='0.1',
+        author="Mounica Pothureddy",
+        author_email="mounicapothureddy555@gmail.com",
+        description="This is tax calculator",
+        packages=setuptools.find_packages(),
+        classifiers= [
+            "Programming Language :: Python :: 3",
+            "Operating System :: MacOS :: MacOS X",
+            "Operating System :: Microsoft :: Windows",
+        ]
+)
